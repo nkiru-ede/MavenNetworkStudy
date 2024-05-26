527e3906b7580e68412cbb55ad45ecbab417122a
Nkiru Ede <39949395+nkiru-ede@users.noreply.github.com>
2024-05-27 10:20:47 +1200

    Update test_aggregate_ga.py

--- tests/test_aggregate_ga.py
+++ tests/test_aggregate_ga.py
@@ -4,7 +4,7 @@ from aggregate_ga import process_merged_GA
 
 class TestGroupingAndAggregation(unittest.TestCase):
     def setUp(self):
-         # file path to the test data (this can be found in the data folder - 'test_data')
+         # file path to the test data (this can be found in the data folder - 'data\test_data')
         self.csv_file_path = "C:\\Users\\edenk\\Downloads\\test_data.csv"
 
     def test_grouping_and_aggregation(self):
@@ -12,7 +12,7 @@ class TestGroupingAndAggregation(unittest.TestCase):
         processed_df = process_merged_GA(self.csv_file_path)
 
        
-        expected_num_rows = 2  
+        expected_num_rows = 3 
 
         
         self.assertEqual(len(processed_df), expected_num_rows)
