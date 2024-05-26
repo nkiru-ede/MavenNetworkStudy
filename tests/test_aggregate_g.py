0a530fe928933a29dffa89e77f98ed81c27f5d62
Nkiru Ede <39949395+nkiru-ede@users.noreply.github.com>
2024-05-26 19:27:31 +1200

    Add files via upload

---
+++ tests/test_aggregate_g.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 26 19:22:19 2024
+
+@author: edenk
+"""
+
+import unittest
+import pandas as pd
+from aggregate_g import process_merged_G
+
+class TestGroupingAndAggregation(unittest.TestCase):
+    def setUp(self):
+        # Specify the file path to your test CSV file
+        self.csv_file_path = "C:\\Users\\edenk\\Downloads\\test_data.csv"
+
+    def test_grouping_and_aggregation(self):
+        
+        processed_df = process_merged_G(self.csv_file_path)
+
+       
+        expected_num_rows = 3  
+
+        
+        self.assertEqual(len(processed_df), expected_num_rows)
+
+if __name__ == '__main__':
+    unittest.main()
