76b0e9c9395fca5c58d19b02e21720057b2440e9
Nkiru Ede <39949395+nkiru-ede@users.noreply.github.com>
2024-05-26 19:31:15 +1200

    Update test_aggregate_ga.py

--- tests/test_aggregate_ga.py
+++ tests/test_aggregate_ga.py
@@ -4,7 +4,7 @@ from aggregate_ga import process_merged_GA
 
 class TestGroupingAndAggregation(unittest.TestCase):
     def setUp(self):
-        # Specify the file path to your test CSV file
+         # file path to the test data (this can be found in the data folder - 'test_data')
         self.csv_file_path = "C:\\Users\\edenk\\Downloads\\test_data.csv"
 
     def test_grouping_and_aggregation(self):
