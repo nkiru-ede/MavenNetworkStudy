25c2857a0eaf28d1bb15c7a2a93674934f098719
Nkiru Ede <39949395+nkiru-ede@users.noreply.github.com>
2024-05-28 20:25:03 +1200

    Add files via upload

--- tests/test_aggregate_g.py
+++ tests/test_aggregate_g.py
@@ -7,22 +7,48 @@ Created on Sun May 26 19:22:19 2024
 
 import unittest
 import pandas as pd
+import os
 from aggregate_g import process_merged_G
 
 class TestGroupingAndAggregation(unittest.TestCase):
     def setUp(self):
-        # file path to the test data (this can be found in the data folder - 'data\test_data')
-        self.csv_file_path = "C:\\Users\\edenk\\Downloads\\test_data.csv"
+          
+        # Specifying the file path to the test CSV file
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        
+        self.csv_file_path = os.path.join(current_dir, "data","GAV", "test_data.csv")
+        
 
     def test_grouping_and_aggregation(self):
+        expected_data = {
+            'Source_Group_Id': ["group1", "group2", "group2"],
+            'Source Release Date': ["1/01/2022", "1/01/2022","1/01/2022"],
+            'Source Release Year': ["1/01", "1/01" ,"1/01" ],
+            'Target_Group_Id': ["group2","group1","group3"],
+            'Target Release Date': ["1/01/2023","1/01/2022","1/01/2022"],
+            'Target Release Year': ["1/01","1/01","1/01"],
+            'Target_First_Release_Date': ["1/01/2023","1/01/2022","1/01/2022"],
+            'Target_Last_Release_Date': ["1/01/2023","1/01/2022","1/01/2022"],
+            'Target_Version_Count': [1,1,1],
+            'Source_First_Release_Date':["1/01/2022","1/01/2022","1/01/2022"],
+            'Source_Last_Release_Date': ["1/01/2022","1/01/2022","1/01/2022"],
+            'Source_Version_Count': [1,1,1]
+            
+        }
+        expected_df = pd.DataFrame(expected_data)
         
         processed_df = process_merged_G(self.csv_file_path)
 
        
         expected_num_rows = 3  
-
+        
         
         self.assertEqual(len(processed_df), expected_num_rows)
+        pd.testing.assert_frame_equal(processed_df, expected_df)
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
