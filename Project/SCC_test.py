d11f5ba4024e6eeca8af5ab4fab986c9fe31f0d4
Nkiru Ede <39949395+nkiru-ede@users.noreply.github.com>
2024-07-18 10:58:47 +1200

    Update SCC_test.py

--- Project/SCC_test.py
+++ Project/SCC_test.py
@@ -1,79 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul  9 16:46:03 2024
-
-@author: edenk
-"""
-
-import os
-import networkx as nx
-import matplotlib.pyplot as plt
+import unittest
 import pandas as pd
+from SCC import compute_sccs
 
-# Sample data
-links_data = {
-    'source': ['A', 'B', 'C', 'D', 'E'],
-    'target': ['B', 'C', 'A', 'E', 'F']
-}
+class TestComputeSCCs(unittest.TestCase):
 
-release_data = {
-    'artifact': ['A', 'B', 'C', 'D', 'E', 'F'],
-    'release': ['2024-01-01', '2023-06-15', '2022-12-31', '2023-09-20', '2024-02-10', '2023-11-05']
-}
+    def test_compute_sccs(self):
+        links_data = """source,target
+                        A,B
+                        B,C
+                        C,A
+                        D,E
+                        E,F
+                        F,D
+                        G,H"""
+        release_data = """artifact,release
+                          A,2024-01-01
+                          B,2024-01-02
+                          C,2024-01-03
+                          D,2024-01-04
+                          E,2024-01-05
+                          F,2024-01-06
+                          G,2024-01-07
+                          H,2024-01-08"""
 
+        links_df = pd.read_csv(pd.compat.StringIO(links_data), delimiter=',')
+        release_df = pd.read_csv(pd.compat.StringIO(release_data), delimiter=',')
 
-new_df = pd.DataFrame(links_data)
-new_dataset = pd.DataFrame(release_data)
+        result = compute_sccs(links_df, release_df)
+        
+        # Expected strongly connected components
+        expected_sccs = [{'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G'}, {'H'}]
+        result_sccs = [set(scc) for scc in result['sccs']]
 
-# Clean and merge data
-new_df.columns = new_df.columns.str.replace('"', '').str.strip()
-new_dataset.columns = new_dataset.columns.str.replace('"', '').str.strip()
+        self.assertCountEqual(result_sccs, expected_sccs)
+        self.assertEqual(result['max_scc_size'], 3)
+        self.assertEqual(result['scc_sizes'], [3, 3, 1, 1])
 
-new_df = new_df.merge(new_dataset[['artifact', 'release']], how='left', left_on='source', right_on='artifact')
-new_df.rename(columns={'release': 'dependency_release_date'}, inplace=True)
-new_df.drop(columns=['artifact'], inplace=True)
-
-new_df = new_df.merge(new_dataset[['artifact', 'release']], how='left', left_on='target', right_on='artifact')
-new_df.rename(columns={'release': 'artifact_release_date'}, inplace=True)
-new_df.drop(columns=['artifact'], inplace=True)
-
-new_df['dependency_release_date'] = new_df['dependency_release_date'].str.replace(r'\[GMT\]', '', regex=True)
-new_df['artifact_release_date'] = new_df['artifact_release_date'].str.replace(r'\[GMT\]', '', regex=True)
-
-new_df['dependency_release_date'] = pd.to_datetime(new_df['dependency_release_date'], format='%Y-%m-%d', errors='coerce')
-new_df['artifact_release_date'] = pd.to_datetime(new_df['artifact_release_date'], format='%Y-%m-%d', errors='coerce')
-
-# Create a directed graph (DAG)
-dag = nx.DiGraph()
-
-for _, row in new_df.iterrows():
-    source = row['source']
-    target = row['target']
-    dag.add_edge(source, target)
-
-
-sccs = list(nx.strongly_connected_components(dag))
-
-
-scc_sizes = [len(scc) for scc in sccs]
-scc_size_counts = pd.Series(scc_sizes).value_counts().sort_index()
-
-
-scc_df = pd.DataFrame({
-    'SCC Size': scc_size_counts.index,
-    'Count': scc_size_counts.values
-})
-
-
-print(scc_df)
-
-
-plt.figure(figsize=(10, 6))
-plt.hist(scc_sizes, bins=range(1, max(scc_sizes) + 2), edgecolor='black')
-plt.xlabel('Size of SCC')
-plt.ylabel('Number of SCCs')
-plt.title('Histogram of Strongly Connected Component Sizes')
-plt.yscale('log') 
-plt.grid(True)
-
-plt.show()
+if __name__ == "__main__":
+    unittest.main()
