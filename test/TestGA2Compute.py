85c8ef40295d5ca704bdc6b2cd5ad1d9fe5510c2
Nkiru Ede <39949395+nkiru-ede@users.noreply.github.com>
2024-08-09 10:25:40 +1200

    Add files via upload

--- test/TestGA2Compute.py
+++ test/TestGA2Compute.py
@@ -1,25 +1,10 @@
 import unittest
 import pandas as pd
-
-def compute_counts(df):
-    df['dependency_release_date'] = pd.to_datetime(df['dependency_release_date'])
-    df['artifact_release_date'] = pd.to_datetime(df['artifact_release_date'])
-
-    earliest_dependency_dates = df.groupby('Dependencies')['dependency_release_date'].min().reset_index()
-    earliest_artifact_dates = df.groupby('Artifact')['artifact_release_date'].min().reset_index()
-
-    earliest_dependency_dates['Year'] = earliest_dependency_dates['dependency_release_date'].dt.year
-    earliest_artifact_dates['Year'] = earliest_artifact_dates['artifact_release_date'].dt.year
-
-    dependency_counts = earliest_dependency_dates.groupby('Year').size().reset_index(name='Count of Dependencies')
-    artifact_counts = earliest_artifact_dates.groupby('Year').size().reset_index(name='Count of Artifacts')
-
-    return dependency_counts, artifact_counts
+from ComputeGA2 import compute_counts
 
 class TestComputeCounts(unittest.TestCase):
     
     def setUp(self):
-      
         self.df = pd.read_csv('./data/test_data.csv')
 
     def test_compute_counts(self):
